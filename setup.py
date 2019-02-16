7d3b94cc44348247fdbd680dfce786a1d600dcaa
Daniel Munch <30357821+Dmunch04@users.noreply.github.com>
2019-02-16 20:16:27 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import multiprocessing
+from setuptools import setup
+
+setup(name='googledocscontentgetter',
+    version='1.0',
+    py_modules=['getgoogledocs'],
+    author='Munchii',
+    author_email='contact@munchii.me',
+    license='MIT',
+    description='This returns the content from a specific google docs',
+    url='https://github.com/Dmunch04/Google-Docs-Python-API',
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+        ],
+    keywords='google docs python api wrapper',
+    )
